5a689ecedd3255b8850dcb847328bee272b41ebb
Paul Strootman <paulstrootman@Pauls-MacBook-Pro.local>
2025-12-01 18:18:18 +0400

    Fix: Restore PMTiles protocol initialization

--- js/app.js
+++ js/app.js
@@ -22,6 +22,8 @@ const STYLES = {
 
 // Initialize the application
 async function init() {
+    // Initialize PMTiles protocol
+    protocol = new pmtiles.Protocol();
     maplibregl.addProtocol("pmtiles", (request, callback) => {
         return protocol.tile(request, callback);
     });
